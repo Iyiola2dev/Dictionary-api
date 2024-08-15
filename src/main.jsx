c5538456f6d378f628650afb2d5d676722991ac6
Lexicon <iyiolad21@gmail.com>
2024-08-15 12:26:50 +0100

    My dictionary is fully ready

--- src/main.jsx
+++ src/main.jsx
@@ -6,12 +6,9 @@ import "./index.css";
 import { BrowserRouter } from "react-router-dom";
 
 createRoot(document.getElementById("root")).render(
-<React.StrictMode>
-<BrowserRouter>
-   
+  <React.StrictMode>
+    <BrowserRouter>
       <App />
-  
-  </BrowserRouter>
-</React.StrictMode>
-  
+    </BrowserRouter>
+  </React.StrictMode>
 );
